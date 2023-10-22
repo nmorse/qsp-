07c0ccdf5e084aa431d44362ee37522505f5fedc
nmorse <morse.nate@gmail.com>
2023-10-22 15:11:46 -0400

    UI is serviceable

--- src/App.tsx
+++ src/App.tsx
@@ -9,40 +9,46 @@ function App() {
 
   return (
     <>
-    <h1>QSP</h1>
+      <h3>Names in a Quantum Super Position</h3>
       <div class="animation-container">
-        {availList().length ? 
+        {availList().length ?
           availList().map((item, i) => { return <h1 class={`name animation${i % 4 + 1}`}>{item}</h1> }) :
-           null }
-          <h1 id="selectedName" class={`name animation-select`}>{pName()}</h1>
+          null}
+        <h1 id="selectedName" class={`name animation-select`}>{pName()}</h1>
       </div>
-      <p>Names in a Quantum Super Position</p>
-      <div style="display:flow:">
-      <textarea id="srcNames" >
-        {`
-renuka
-val
-dhav
-`}
-      </textarea>
-      <button
-        onClick={() => {
-          const names = document.getElementById('srcNames').value.split(/\s*\n\s*/).filter((i) => i.length > 0)
-          setAvailList(names)
-          setComplete([])
-          setPName('')
-        }
-        }>
-        set
-      </button>
+
+      <p style={`display:${availList().length === 0 ? "block" : "none"}`}>Setup the list of names with one name per line.</p>
+      <div class="setupform" style={`display:${availList().length === 0 ? "flex" : "none"}`}>
+        <textarea id="srcNames" >
+          {`
+Renuka
+Val
+Dhav
+Nicko
+Chris
+Ruian
+Curt
+Nate`}
+        </textarea>
+        <button
+          onClick={() => {
+            const names = document.getElementById('srcNames').value.split(/\s*\n\s*/).filter((i) => i.length > 0)
+            setAvailList(names)
+            setComplete([])
+            setPName('')
+          }
+          }>
+          setup
+        </button>
       </div>
+
 
       <div class="card">
         <button
           onClick={() => {
             const sn = document.getElementById('selectedName')
             let anim = ''
-            if(sn) {
+            if (sn) {
               anim = sn.style.animation
               sn.style.animation = 'none'
               // sn.className = 'name animation-none'
@@ -56,25 +62,31 @@ dhav
               setAvailList((o) => o.filter((i) => i !== randomName))
               setCount((count) => count + 1)
               setComplete((o) => o.concat([randomName]))
-              setTimeout(()=> {
-              if(sn) {
-                sn.style.animation = "4s linear 0s nameAnimationSelect"
-                sn.style.animationFillMode = "forwards"
-                sn.className = 'name animation-select'
-              }
+              setTimeout(() => {
+                if (sn) {
+                  sn.style.animation = "1s ease-out nameAnimationSelect"
+                  sn.style.animationFillMode = "forwards"
+                  sn.className = 'name animation-select'
+                }
               }, 50)
             }
           }}>
-          {Math.random()>0.5? 'make an observation' : 'collapse the wave function'} {count()}
+          { count()%3 === 0 ? 'make an measurment' :  count()%3 === 1 ?  'collapse the wave function': 'observe the quantum state'}
         </button>
-        <p>{pName}</p>
-        <hr />
-        <div>
-          {availList().map((i) => { return <div>{i}</div> })}
-        </div>
-        <hr />
-        <div>
-          {complete().map((i) => { return <div>{i}</div> })}
+        <div class="name-columns" >
+        <div class="name-list">
+          <div>yet to be selected:</div>
+            {availList().map((i) => { return <div>{i}</div> })}
+          </div>
+          <div class="name-list">
+            <div>the one:</div>
+            <div>{pName}</div>
+          </div>
+          
+          <div class="name-list">
+            <div>selected in this orded:</div>
+            {complete().map((i) => { return <div>{i}</div> })}
+          </div>
         </div>
 
       </div>
