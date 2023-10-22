10fa1590f1ede156a23a06e50cd143920cdc8b77
nmorse <morse.nate@gmail.com>
2023-10-22 09:29:58 -0400

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,38 +8,40 @@ function App() {
   const [pName, setPName] = createSignal('')
   const [availList, setAvailList] = createSignal([])
   const [complete, setComplete] = createSignal([])
-  
+
   return (
     <>
-
-      <h1>QSP</h1>
+      <div class="animation-container">
+        {availList().map((item, i) => { return <h1 class={"name animation" + (i%3 + 1)}>{item}</h1> })}
+      </div>
+      {/* <h1>QSP</h1> */}
       <textarea id="srcNames" >
-      {`
+        {`
 renuka
 val
 dhav
       `}
       </textarea>
       <button
-          onClick={() => {
-            const names = document.getElementById('srcNames').value.split(/\s*\n\s*/).filter((i) => i.length > 0)
-            console.log(names)
-            setAvailList(names)
-            setComplete([])
-            setPName('')
-          }
+        onClick={() => {
+          const names = document.getElementById('srcNames').value.split(/\s*\n\s*/).filter((i) => i.length > 0)
+          console.log(names)
+          setAvailList(names)
+          setComplete([])
+          setPName('')
+        }
         }>
-          set
-        </button>
+        set
+      </button>
 
       <div class="card">
         <button
           onClick={() => {
             const av = availList()
             if (av.length) {
-              const ri = Math.floor(Math.random()*av.length) 
-              console.log(av.slice(ri, ri+1));
-              const randomName = av.slice(ri, ri+1)[0]
+              const ri = Math.floor(Math.random() * av.length)
+              console.log(av.slice(ri, ri + 1));
+              const randomName = av.slice(ri, ri + 1)[0]
               setPName(() => randomName)
               console.log(av)
               setAvailList((o) => o.filter((i) => i !== randomName))
