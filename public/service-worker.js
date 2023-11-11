68ca8286724abb34f4445527c8c3865f20f3d14c
nmorse <morse.nate@gmail.com>
2023-11-11 08:17:27 -0500

    cache with refresh

---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+const cacheName = "qspCache"
+const precachedResources = ["/", "/index.html"];
+
+async function precache() {
+    const cache = await caches.open(cacheName);
+    return cache.addAll(precachedResources);
+}
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(precache());
+});
+
+function isCacheable(request) {
+    const url = new URL(request.url);
+    return !url.pathname.endsWith(".json");
+  }
+  
+  async function cacheFirstWithRefresh(request) {
+    const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
+      if (networkResponse.ok) {
+        const cache = await caches.open(cacheName);
+        cache.put(request, networkResponse.clone());
+      }
+      return networkResponse;
+    });
+  
+    return (await caches.match(request)) || (await fetchResponsePromise);
+  }
+  
+  self.addEventListener("fetch", (event) => {
+    if (isCacheable(event.request)) {
+      event.respondWith(cacheFirstWithRefresh(event.request));
+    }
+  });
