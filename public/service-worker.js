8982c5a775cc2acec9b6142a17d42d6c37c72dff
Nate Morse <morse.nate@gmail.com>
2024-04-13 16:09:48 -0400

    should clean up files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,8 @@
 const cacheName = "qspCache"
-const precachedResources = ["/", "/index.html"];
+const precachedResources = ["/", "/index.html",
+"/init-service-worker.js", 
+"/assets/index-a9145643.css", 
+"/assets/index-571dabb8.js"];
 
 async function precache() {
     const cache = await caches.open(cacheName);
@@ -13,22 +16,22 @@ self.addEventListener("install", (event) => {
 function isCacheable(request) {
     const url = new URL(request.url);
     return !url.pathname.endsWith(".json");
-  }
-  
-  async function cacheFirstWithRefresh(request) {
+}
+
+async function cacheFirstWithRefresh(request) {
     const fetchResponsePromise = fetch(request).then(async (networkResponse) => {
-      if (networkResponse.ok) {
-        const cache = await caches.open(cacheName);
-        cache.put(request, networkResponse.clone());
-      }
-      return networkResponse;
+        if (networkResponse.ok) {
+            const cache = await caches.open(cacheName);
+            cache.put(request, networkResponse.clone());
+        }
+        return networkResponse;
     });
-  
+
     return (await caches.match(request)) || (await fetchResponsePromise);
-  }
-  
-  self.addEventListener("fetch", (event) => {
+}
+
+self.addEventListener("fetch", (event) => {
     if (isCacheable(event.request)) {
-      event.respondWith(cacheFirstWithRefresh(event.request));
+        event.respondWith(cacheFirstWithRefresh(event.request));
     }
-  });
+});
